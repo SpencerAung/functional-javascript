893465a18510dcacfb55b1c5f4de01ec3bbf903b
susuaung <susuaung.ms@gmail.com>
2016-10-31 10:42:13 +0900

    minyear es2 version

--- minyear.js
+++ minyear.js
@@ -3,6 +3,10 @@ function minyear(years){
         return (pre<cur)? pre:cur;
     });
 }
+/* es6 , with the use of arrow function*/
+// function minyear(years){
+//     return years.reduce((pre,cur)=>(pre<cur)?pre:cur);
+// }
 
 yearList = [1991,2000,2006,2020];
 console.log(minyear(yearList));
